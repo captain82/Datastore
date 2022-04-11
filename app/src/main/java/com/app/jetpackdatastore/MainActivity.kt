6be990a2c76ecf89fce67db6e339f7315e8bff93
ankitkr82 <ankit.kumar@rapido.bike>
2022-04-11 09:33:06 +0530

    Only shared pref impl added

--- app/src/main/java/com/app/jetpackdatastore/MainActivity.kt
+++ app/src/main/java/com/app/jetpackdatastore/MainActivity.kt
@@ -1,17 +1,8 @@
 package com.app.jetpackdatastore
 
-import android.content.Context
 import android.os.Bundle
-import android.preference.PreferenceDataStore
-import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.datastore.core.DataStore
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.util.prefs.Preferences
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,23 +14,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         sharedPref = SharedPreferencesHelper(this)
         textView = findViewById(R.id.text)
+        sharedPref.setName()
+        sharedPref.setRole()
 
-        getDataFromSharedPrefAsync()
-    }
+        textView.text = "Name: ${sharedPref.getName()} \nRole: ${sharedPref.getRole()}"
 
-    private fun getDataFromSharedPrefSync() {
-        textView.text = "Name: ${sharedPref.nameFromDataStore} \nRole: ${sharedPref.roleFromDataStore}"
-    }
-
-    private fun getDataFromSharedPrefAsync() {
-        lifecycleScope.launch(Dispatchers.IO) {
-            Log.i("activityThread","${Thread.currentThread().name}")
-            val name = sharedPref.getNameFromDS()
-            val role = sharedPref.getRoleFromDS()
-            withContext(Dispatchers.Main){
-                Log.i("activityThread","${Thread.currentThread().name}")
-                textView.text =  "Name: ${name} \nRole: ${role}"
-            }
-        }
     }
 }
--- app/src/main/java/com/app/jetpackdatastore/SharedPreferencesHelper.kt
+++ app/src/main/java/com/app/jetpackdatastore/SharedPreferencesHelper.kt
@@ -1,39 +1,14 @@
 package com.app.jetpackdatastore
 
 import android.content.Context
-import android.util.Log
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.SharedPreferencesMigration
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.emptyPreferences
-import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
-import java.io.IOException
 
-private val Context._dataStore by preferencesDataStore(
-    name = SharedPrefConstants.DATASTORE_NAME,
-    produceMigrations = { context ->
-        listOf(
-            SharedPreferencesMigration(context,
-                SharedPrefConstants.PREFERENCES_NAME))
-    }
-)
+
 class SharedPreferencesHelper constructor(private val context: Context) {
 
     private val preferences = context.getSharedPreferences(
         SharedPrefConstants.PREFERENCES_NAME,
         Context.MODE_PRIVATE
     )
-
-    val dataStore: DataStore<Preferences> = context._dataStore
 
     private var editor = preferences.edit()
 
@@ -52,62 +27,4 @@ class SharedPreferencesHelper constructor(private val context: Context) {
     fun getRole(): String {
         return preferences.getString(SharedPrefConstants.ROLE,"") ?: ""
     }
-
-    var nameFromDataStore: String
-        get() = runBlocking{
-            withContext(Dispatchers.Default) {
-                dataStore.getValueFlow(stringPreferencesKey(SharedPrefConstants.NAME), "").first()
-            }
-        }
-
-        set(value) = runBlocking {
-            withContext(Dispatchers.Default){
-                dataStore.edit {
-                    it[stringPreferencesKey(SharedPrefConstants.NAME)] = value
-                }
-            }
-        }
-
-    var roleFromDataStore: String
-        get() = runBlocking{
-            withContext(Dispatchers.Default) {
-                dataStore.getValueFlow(stringPreferencesKey(SharedPrefConstants.ROLE), "").first()
-            }
-        }
-
-        set(value) = runBlocking {
-            withContext(Dispatchers.Default){
-                dataStore.edit {
-                    it[stringPreferencesKey(SharedPrefConstants.ROLE)] = value
-                }
-            }
-        }
-
-    suspend fun getNameFromDS(): String {
-        Log.i("PrefThread","${Thread.currentThread().name}")
-        return dataStore.getValueFlow(stringPreferencesKey(SharedPrefConstants.NAME),"").first()
-
-    }
-
-    suspend fun getRoleFromDS(): String {
-        return dataStore.getValueFlow(stringPreferencesKey(SharedPrefConstants.ROLE),"").first()
-
-    }
-
-    fun <T> DataStore<Preferences>.getValueFlow(
-        key: Preferences.Key<T>,
-        defaultValue: T,
-    ): Flow<T> {
-        return this.data
-            .catch { exception ->
-                if (exception is IOException) {
-                    emit(emptyPreferences())
-                } else {
-                    throw exception
-                }
-            }.map { preferences ->
-                preferences[key] ?: defaultValue
-            }
-    }
-
 }
